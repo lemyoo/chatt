8941c6ef226dc1ad7c3bb0fe4692e61c6d4a9d22
Emmanuel Lartey <emmanuel.lartey@accertix.com>
2021-02-15 09:35:28 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+    console.log('User is connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected')
+    })
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message', msg)
+    })
+})
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
